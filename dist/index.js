41f1fd49a5b30b264f354648e8df2ad9b7843945
anasmak04 <anasdev8@gmail.com>
2023-08-19 23:30:20 +0100

    added type annotations with variables & arrays

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
-let namee = "hello";
-console.log(namee);
+let theName = "Anas";
+let theAge = 22;
+let Hired = true;
+let all = undefined;
+all = 80;
+all = "Hello";
+console.log(theName);
+console.log(theAge);
+console.log(Hired);
+console.log(all);
+let arrayStrings = ["Jhon", "Smith"];
+let arrayNumbers;
+arrayNumbers = [1, 2, 3];
+let arrayBooleans = [true, false];
+let arrayMulti = ["jhon", 22, ["hello"]];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-let namee : String = "hello";
-console.log(namee);
+// Type Annotation ***************************
+let theName: String = "Anas";
+let theAge: number = 22;
+let Hired: boolean = true;
+let all: any = undefined;
+all = 80;
+all = "Hello";
+console.log(theName);
+console.log(theAge);
+console.log(Hired);
+console.log(all);
+// Type Annotations With Arrays ***************************
+let arrayStrings: String[] = ["Jhon", "Smith"];
+let arrayNumbers: number[];
+arrayNumbers = [1, 2, 3];
+let arrayBooleans: boolean[] = [true, false];
+// Type Annotations With Arrays Multidimensional  ***************************
+let arrayMulti: (String | Number | String[])[] = ["jhon", 22, ["hello"]];
